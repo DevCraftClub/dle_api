14ed65077846140fe1451dd713e8a08575918097
MaximHarder <19776684+MaximHarder@users.noreply.github.com>
2026-08-26 10:48:25 +0000

    chore(openapi): regenerate openapi.yaml

--- upload/api/vendor/zircote/swagger-php/src/Attributes/Server.php
+++ upload/api/vendor/zircote/swagger-php/src/Attributes/Server.php
@@ -7,29 +7,30 @@
 namespace OpenApi\Attributes;
 
 use OpenApi\Annotations as OA;
-use OpenApi\Generator;
+use OpenApi\Undefined;
 
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE)]
 class Server extends OA\Server
 {
     /**
-     * @param ServerVariable[]         $variables
+     * @param list<ServerVariable>     $variables
      * @param array<string,mixed>|null $x
-     * @param Attachable[]|null        $attachables
+     * @param list<Attachable>|null    $attachables
      */
     public function __construct(
         ?string $url = null,
-        ?string $description = Generator::UNDEFINED,
+        ?string $description = Undefined::UNDEFINED,
         ?array $variables = null,
-        // annotation
+
+        // abstract annotation
         ?array $x = null,
         ?array $attachables = null
     ) {
         parent::__construct([
-                'url' => $url ?? Generator::UNDEFINED,
+                'url' => $url ?? Undefined::UNDEFINED,
                 'description' => $description,
-                'x' => $x ?? Generator::UNDEFINED,
-                'attachables' => $attachables ?? Generator::UNDEFINED,
+                'x' => $x ?? Undefined::UNDEFINED,
+                'attachables' => $attachables ?? Undefined::UNDEFINED,
                 'value' => $this->combine($variables),
             ]);
     }
